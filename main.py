bcac058935351ec35915b7e8c65dfb1433f9a8a3
Jlferrete <joseluisferrete@gmail.com>
2023-06-25 13:45:39 +0200

    Initial configuration

---
+++ main.py
@@ -0,0 +1,11 @@
+from custom_webdriver.custom_webdriver import driver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def main():
+    print("Starting to scrape for downloading")
+
+
+if __name__ == '__main__':
+    main()
